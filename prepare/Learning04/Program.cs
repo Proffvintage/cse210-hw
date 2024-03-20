023c512823ca4f139999d840a15f6f6a642ce2c6
Proffvintage <145012191+Proffvintage@users.noreply.github.com>
2024-03-20 07:03:00 +0100

    Update Program.cs

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        MathAssignment JakeMath = new MathAssignment("Jacob Lamb", "Math 215","Section 7","Problem 7-21");
-        WrittingAssignment JakeWritting = new WrittingAssignment("Jacob Lamb", "ENG 105", "The Pros and Cons of medical marijuana");
+        MathAssignment MarksonMath = new MathAssignment("Samuel Markson", "Math 215","Section 7","Problem 7-21");
+        WrittingAssignment JakeWritting = new WrittingAssignment("Markson Samuel", "ENG 105", "The Pros and Cons of Tobacco");
         Console.WriteLine(JakeMath.getSummary());
         Console.WriteLine(JakeMath.getMathHomeWork());
         Console.WriteLine(JakeWritting.getSummary());
